abf4091fdb983ce74d0a2dae90e66e831e8882dd
Dolevkle <dolevkle@gmail.com>
2024-04-28 01:00:04 +0300

    drawer functionality works need to make options work and map also rendered

--- app/src/main/java/com/example/advizors/MainActivity.kt
+++ app/src/main/java/com/example/advizors/MainActivity.kt
@@ -1,44 +1,40 @@
+
 package com.example.advizors
 
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity() {
-    lateinit var drawerLayout: DrawerLayout
-    lateinit var toolbar: Toolbar
+
+
+
+    private lateinit var drawer: DrawerLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // init drawer layout
-        drawerLayout = findViewById(R.id.drawer_layout)
-        toolbar = findViewById(R.id.toolbar);
-        // init action bar drawer toggle
-        // add a drawer listener into  drawer layout
+        setSupportActionBar(findViewById(R.id.toolbar))
+        drawer = findViewById(R.id.drawer_layout)
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
 
-        // show menu icon and back icon while drawer open
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // check conndition for drawer item with menu item
-//        return if (actionBarDrawerToggle.onOptionsItemSelected(item)){
-//            true
-//        }else{
-//            super.onOptionsItemSelected(item)
-//        }
-//
-//    }
-}
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
+}
